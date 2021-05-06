fecb5d023847b817b43e72cac7aad542f00272eb
LourivalJunior27 <80164813+LourivalJunior27@users.noreply.github.com>
2021-05-05 23:04:30 -0300

    Delete P2Upgrade directory

--- P2Upgrade/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$route = explode("/", $_GET['url'] ?? 'home');
-
-// var_dump($route);
-
-if (file_exists("pages/{$route[0]}.php")) {
-  include "pages/{$route[0]}.php";
-}
--- P2Upgrade/pages/cadastro/ler.php
+++
@@ -1,9 +0,0 @@
-<?php //LER
-
-function ler() {
-  $sql = 'select * from usuarios';
-  $connection = connection();
-  $result = $connection->prepare($sql);
-  $result->execute();
-  return array_reverse($result->fetchAll(PDO::FETCH_OBJ));
-};
